f9cff3362ecaf615559b6968024ef16163a87721
joana.socrates <joana.socrates@ocado.com>
2019-12-29 00:39:16 +0100

    Parallel Stream Benchmark

--- src/main/benchmark/ParallelStreamBenchmark.java
+++ src/main/benchmark/ParallelStreamBenchmark.java
@@ -1,32 +1,38 @@
+package benchmark;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.apache.commons.lang3.RandomStringUtils.*;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
-@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
-//@Warmup(iterations = 3)
-//@Measurement(iterations = 8)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
+@Warmup(iterations = 5)
+@Measurement(iterations = 10)
 public class ParallelStreamBenchmark {
 
     @Param({"10000000"})
     private int N;
 
-    private List<String> DATA_FOR_TESTING;
+    private List<String> someStrings;
 
     public static void main(String[] args) throws RunnerException {
 
@@ -40,7 +46,7 @@ public class ParallelStreamBenchmark {
 
     @Setup
     public void setup() {
-        DATA_FOR_TESTING = createData();
+        someStrings = createListOfStrings();
     }
 
     @Benchmark
@@ -49,5 +55,9 @@ public class ParallelStreamBenchmark {
             String s = DATA_FOR_TESTING.get(i); //take out n consume, fair with foreach
             bh.consume(s);
         }
+    }
+
+    private List<String> createListOfStrings(){
+        String aString = randomAlphabetic(64);
     }
 }
