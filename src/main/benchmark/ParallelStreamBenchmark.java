8140f13e168177f57ce2ef2b2ce417305aaa1d42
joana.socrates <joana.socrates@ocado.com>
2019-12-29 17:34:43 +0100

    Parallel Stream Benchmark

--- src/main/benchmark/ParallelStreamBenchmark.java
+++ src/main/benchmark/ParallelStreamBenchmark.java
@@ -23,46 +23,77 @@ import org.openjdk.jmh.annotations.Warmup;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@State(Scope.Benchmark)
 @Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
 @Warmup(iterations = 2)
 @Measurement(iterations = 2)
 public class ParallelStreamBenchmark {
 
-    @Param({"100", "1500", "500000"})
-    private int numberOfElements;
-
-    private List<String> someStrings;
-
-    //Creates a new List of random Strings every time the benchmark method is invoked
-    @Setup(Level.Invocation)
-    public void setup() {
-        someStrings = createListOfStrings();
-    }
-
     @Benchmark
-    public List<String> sequentialStream() {
-        return someStrings.stream()
+    public List<String> sequentialStream(GivenState state) {
+        return state.someStrings.stream()
                 .filter(string -> string.length() > 5)
                 .peek(string -> string.replace(string.charAt(5), 'z'))
                 .collect(Collectors.toList());
     }
 
     @Benchmark
-    public List<String> parallelStream() {
-        return someStrings.parallelStream()
+    public List<String> parallelStream(GivenState state) {
+        return state.someStrings.parallelStream()
                 .filter(string -> string.length() > 5)
                 .peek(string -> string.replace(string.charAt(5), 'z'))
                 .collect(Collectors.toList());
     }
 
-    private List<String> createListOfStrings() {
-        List<String> list = new ArrayList<>();
-        Random r = new Random();
-        for (int index = 0; index < numberOfElements; index++) {
-            list.add(randomAlphabetic(r.nextInt(10) + 1));
+    @Benchmark
+    public List<String> sequentialDistinctStream(GivenState state) {
+        return state.someStrings.stream()
+                .distinct()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @Benchmark
+    public List<String> parallelDistinctStream(GivenState state) {
+        return state.someStrings.parallelStream()
+                .distinct()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @Benchmark
+    public List<String> parallelDistinctUnorderedStream(GivenState state) {
+        return state.someStrings.parallelStream()
+                .distinct()
+                .unordered()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @State(Scope.Benchmark)
+    public static class GivenState {
+
+        @Param({"100", "1500", "500000"})
+        private int numberOfElements;
+
+        public List<String> someStrings;
+
+        //Creates a new List of random Strings every time the benchmark method is invoked
+        @Setup(Level.Invocation)
+        public void setUp() {
+            someStrings = createListOfStrings();
         }
-        return list;
+
+        private List<String> createListOfStrings() {
+            List<String> list = new ArrayList<>();
+            Random r = new Random();
+            for (int index = 0; index < numberOfElements; index++) {
+                list.add(randomAlphabetic(r.nextInt(10) + 1));
+            }
+            return list;
+        }
     }
 
 }
