e6b7d002d43f84c3b7b28ad9112156c631669204
joana.socrates <joana.socrates@ocado.com>
2019-12-29 14:34:30 +0100

    Parallel Stream Benchmark

--- src/main/benchmark/BenchmarkRunner.java
+++ src/main/benchmark/BenchmarkRunner.java
@@ -1,4 +1,4 @@
-package benchmark;
+package main.benchmark;
 
 public class BenchmarkRunner {
 
--- src/main/benchmark/ParallelStreamBenchmark.java
+++ src/main/benchmark/ParallelStreamBenchmark.java
@@ -1,20 +1,36 @@
-package benchmark;
+package main.benchmark;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Warmup(iterations = 3)
+@Measurement(iterations = 8)
 public class ParallelStreamBenchmark {
 
     @Param({"100", "1500", "500000"})
-    private int numberOfElements =10;
+    private int numberOfElements = 10;
 
     private List<String> someStrings;
 
@@ -24,8 +40,11 @@ public class ParallelStreamBenchmark {
     }
 
     @Benchmark
-    public void loopFor() {
-        System.out.println(someStrings.get(0));
+    public void parallelStream() {
+         someStrings.stream()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
 
     }
 
