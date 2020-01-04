0b971525ae470ad2ed89fcda010f706c31b1a99f
joana.socrates <joana.socrates@ocado.com>
2020-01-04 21:21:15 +0100

    Benchmark--enhancements

--- src/main/benchmark/ParallelStreamThreads.java
+++ src/main/benchmark/ParallelStreamThreads.java
@@ -13,8 +13,6 @@ import org.openjdk.jmh.annotations.Threads;
 
 public class ParallelStreamThreads {
 
-
-
     @Threads(SMALL_NUMBER_OF_THREADS)
     @Benchmark
     public List<String> sequentialStreamSmallNoThreads(BuildState.GivenState state) {
@@ -46,6 +44,42 @@ public class ParallelStreamThreads {
     @Benchmark
     public List<String> sequentialStreamLargeNoThreads(BuildState.GivenState state) {
         return state.someStrings.stream()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @Threads(SMALL_NUMBER_OF_THREADS)
+    @Benchmark
+    public List<String> parallelStreamSmallNoThreads(BuildState.GivenState state) {
+        return state.someStrings.parallelStream()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @Threads(EXACT_NUMBER_OF_THREADS)
+    @Benchmark
+    public List<String> parallelStreamExactlNoThreads(BuildState.GivenState state) {
+        return state.someStrings.parallelStream()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @Threads(JUST_ABOVE__NUMBER_OF_THREADS)
+    @Benchmark
+    public List<String> parallelStreamAboveNoThreads(BuildState.GivenState state) {
+        return state.someStrings.parallelStream()
+                .filter(string -> string.length() > 5)
+                .peek(string -> string.replace(string.charAt(5), 'z'))
+                .collect(Collectors.toList());
+    }
+
+    @Threads(LARGE_NUMBER_OF_THREADS)
+    @Benchmark
+    public List<String> parallelStreamLargeNoThreads(BuildState.GivenState state) {
+        return state.someStrings.parallelStream()
                 .filter(string -> string.length() > 5)
                 .peek(string -> string.replace(string.charAt(5), 'z'))
                 .collect(Collectors.toList());
